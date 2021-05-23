58949faba240a347f0c3544523050353205a5ad6
taikuri-T <47896768+tommieemeli@users.noreply.github.com>
2021-05-23 20:10:24 +0300

    progress / bensankulutus

--- client/automittari/src/App.js
+++ client/automittari/src/App.js
@@ -3,20 +3,55 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import InputGroup from "react-bootstrap/InputGroup";
-import Dropdown from "react-bootstrap/Dropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import Card from "react-bootstrap/Card";
-import Nav from "react-bootstrap/Nav";
-import Tabs from "react-bootstrap/Tabs";
 import CardDeck from "react-bootstrap/CardDeck";
 import Table from "react-bootstrap/Table";
 
 import "./App.css";
 
+
 function App() {
+
+  const [data, setData] = useState({distance: '', speed1: '', speed2: ''});
+  const [car, setCar] = useState('');
+
+  const inputChanged = (event) => {
+    setData({...data, [event.target.name]: event.target.value})
+  }
+
+  const travelTime1 = (data.distance/data.speed1);
+  const travelTime2 = (data.distance/data.speed2);
+
+
+  const handleDropdown = (e) => {
+    setCar(e.target.value)
+  }
+
+  const k = 1.009;
+  const kulutus = Math.pow(k, data.speed1)
+  const kulutus2 = Math.pow(k, data.speed2)
+  var cartype = "";
+
+  if(car === "3") {
+    cartype = "Auto A";
+  } else if(car === "3.5") {
+    cartype = "Auto B";
+  } else if(car === "4") {
+    cartype = "Auto C";
+  }
+
+  const showData = () => {
+    console.log("Matka: " + data.distance + "km " + "\n" + "Nopeus1: " + data.speed1 + "km/h" + "\n" + "Nopeus2: " + data.speed2 + "km/h" + "\n" +
+    "Auto: " + cartype + ", auton kulutus: " + car + " L/100km" + "\n" + 
+    "Matka aika1: " + (travelTime1*60) + " minuuttia " + "\n" + "Matka aika2: " + (travelTime2*60) + " minuuttia" + "\n" + 
+    "Kulutus valitulla autolla ja nopeudella1: " + (car*kulutus) + "\n" + 
+    "Kulutus valitulla autolla ja nopeudella2: " + (car*kulutus2));
+  }
+
+  
+
   return (
     <div className="App">
       <Container>
@@ -35,14 +70,18 @@ function App() {
                     <FormControl
                       type="number"
                       placeholder="Matka kilometreinä"
+                      value={data.distance}
+                      name="distance"
+                      onChange={inputChanged}
                     />
                   </Form.Group>
                   <Form.Group controlId="car">
                     <Form.Label>AUTO</Form.Label>
-                    <Form.Control as="select" custom>
-                      <option>Auto A</option>
-                      <option>Auto B</option>
-                      <option>Auto C</option>
+                    <Form.Control as="select" custom onChange={handleDropdown}>
+                      <option>Valitse..</option>
+                      <option value="3">Auto A</option>
+                      <option value="3.5">Auto B</option>
+                      <option value="4">Auto C</option>
                     </Form.Control>
                   </Form.Group>
                 </Form.Row>
@@ -53,6 +92,8 @@ function App() {
                       <FormControl
                         type="number"
                         placeholder="Nopeus (km/h)"
+                        name="speed1"
+                        onChange={inputChanged}
                       ></FormControl>
                     </Form.Group>
                   </Col>
@@ -62,11 +103,13 @@ function App() {
                       <FormControl
                         type="number"
                         placeholder="Nopeus (km/h)"
+                        name="speed2"
+                        onChange={inputChanged}
                       ></FormControl>
                     </Form.Group>
                   </Col>
                 </Form.Row>
-                <Button type="submit" variant="danger">
+                <Button onClick={showData} variant="danger">
                   LASKE !
                 </Button>
               </Form>
@@ -78,29 +121,29 @@ function App() {
             <Table striped bordered hover variant="dark">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>AUTO</th>
+                  <th>Matka-aika(min)</th>
+                  <th>Bensankulutus</th>
+                  <th>ASD</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <td>Nopeus 1</td>
+                  <td></td>
+                  <td></td>
+                  <td>ASD</td>
                 </tr>
                 <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
+                  <td>Nopeus 2</td>
+                  <td></td>
+                  <td></td>
+                  <td>ASD</td>
                 </tr>
                 <tr>
                   <td>3</td>
                   <td colSpan="2">Larry the Bird</td>
-                  <td>@twitter</td>
+                  <td>ASD</td>
                 </tr>
               </tbody>
             </Table>
@@ -116,6 +159,7 @@ function App() {
                     <Card.Title>AUTOT</Card.Title>
                     <Card.Text>
                       <b>KULUTUS</b>
+                      <p>Kulutus on mitattu 1km/h vauhdissa, kulutus kasvaa 1,009 kertoimella per 1km/h</p>
                       <li>Auto A: 3l / 100km</li>
                       <li>Auto B: 3,5l / 100km</li>
                       <li>Auto C: 4l / 100km</li>
